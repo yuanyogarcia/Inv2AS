3b5a0191810269d8b15cb3f61f06898ec3c49d9e
Jaime Ojeda <j.ojeda.gonzalez@gmail.com>
2019-05-03 20:23:55 +0200

    Comentando código

---
+++ src/Casilla.java
@@ -0,0 +1,51 @@
+/*
+ Monopoly para cliente Inv2AS”
+ Proyectos Informáticos II - 2019
+ Grupo C (Juan Jose Garcia, Manuel Angel Mateos, Jaime Ojeda)
+ */
+
+public class Casilla {
+  
+  private int pos;
+  private char tipo;
+  private String name;				
+  private Casilla siguiente; 
+
+  	public Casilla(){}
+  	
+  	public Casilla(char tipo, String name, int pos){ 
+  		this.tipo = tipo;
+  		this.name = name;		
+  		this.pos = pos;
+  	}
+
+	public void ponerPosicion(int pos) {
+	  this.pos = pos;
+  	}
+  	public int obtenerPosicion() {
+  		return pos;
+  	}
+	public void ponerTipo(char tipo) {
+		this.tipo = tipo;
+	}
+	
+	public char obtenerTipo() {
+		return tipo;
+	}
+	
+	public void ponerNombre(String name) {
+		this.name = name;
+	}
+	
+	public String obtenerNombre() {
+		return name;
+	}
+	
+	public void ponerSiguiente(Casilla siguiente) {
+		this.siguiente = siguiente;
+	}
+	public Casilla obtenerSiguiente() {
+		return siguiente;
+	}	
+
+}
