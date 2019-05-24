b5b9316e16f1c8eda92120d6ad14d37737698ec6
Jaime Ojeda <j.ojeda.gonzalez@gmail.com>
2019-05-24 15:52:46 +0200

    Implementación de la Carta Casilla

---
+++ src/CartaCasilla.java
@@ -0,0 +1,13 @@
+/*
+ Monopoly para cliente Inv2AS”
+ Proyectos Informáticos II - 2019
+ Grupo C (Juan Jose Garcia, Manuel Angel Mateos, Jaime Ojeda)
+ */
+
+public class CartaCasilla extends Casilla{
+
+	public CartaCasilla(char tipo, String name, int pos) { //Constructor de la carta casilla
+		super(tipo, name, pos);
+	}
+
+}
