715a77e8dd3869706cc09b21654b8ef948414dab
Jaime Ojeda <j.ojeda.gonzalez@gmail.com>
2019-05-25 16:18:26 +0200

    Creación de la casilla de salida

---
+++ src/SalidaCasilla.java
@@ -0,0 +1,13 @@
+/*
+ Monopoly para cliente Inv2AS”
+ Proyectos Informáticos II - 2019
+ Grupo C (Juan Jose Garcia, Manuel Angel Mateos, Jaime Ojeda)
+ */
+
+public class SalidaCasilla extends Casilla{
+
+	public SalidaCasilla(char tipo, String name, int pos) { //constructor para Salida
+		super(tipo, name, pos);
+	}
+	
+}
