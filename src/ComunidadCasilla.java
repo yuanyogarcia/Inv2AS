4196536c7a9569cb2fc83ba1de8cd1a936a4baec
Jaime Ojeda <j.ojeda.gonzalez@gmail.com>
2019-05-25 12:50:58 +0200

    Implementación de casilla Comunidad

---
+++ src/ComunidadCasilla.java
@@ -0,0 +1,12 @@
+/*
+ Monopoly para cliente Inv2AS”
+ Proyectos Informáticos II - 2019
+ Grupo C (Juan Jose Garcia, Manuel Angel Mateos, Jaime Ojeda)
+ */
+
+public class ComunidadCasilla extends CartaCasilla{
+
+	public ComunidadCasilla(char tipo, String name, int pos) { 
+		super(tipo, name, pos);
+	}
+}
