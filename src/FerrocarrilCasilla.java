5470a84b357c029d64aee04db90857d29d259d58
Jaime Ojeda <j.ojeda.gonzalez@gmail.com>
2019-05-26 14:27:53 +0200

    Implementación de la clase Ferrocarril y su instancia en el tablero

---
+++ src/FerrocarrilCasilla.java
@@ -0,0 +1,11 @@
+/*
+ Monopoly para cliente Inv2AS”
+ Proyectos Informáticos II - 2019
+ Grupo C (Juan Jose Garcia, Manuel Angel Mateos, Jaime Ojeda)
+ */
+
+public class FerrocarrilCasilla extends PropiedadCasilla{
+	public FerrocarrilCasilla(char tipo, String nombre, String color, String precio, String costeConstruccion, String alquiler, String alquilerUnaCasa, int pos){
+  		super(tipo, nombre, color, precio, costeConstruccion, alquiler, alquilerUnaCasa, pos);
+  	}
+}
