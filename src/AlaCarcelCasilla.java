5b18432490a9bf0991662f65b25fde71e893eb94
Jaime Ojeda González <j.ojeda.gonzalez@gmail.com>
2019-06-07 08:58:47 +0200

    creación de clase a la cárcel

---
+++ src/AlaCarcelCasilla.java
@@ -0,0 +1,12 @@
+/*
+ Monopoly para cliente Inv2AS”
+ Proyectos Informáticos II - 2019
+ Grupo C (Juan Jose Garcia, Manuel Angel Mateos, Jaime Ojeda)
+ */
+
+public class AlaCarcelCasilla extends Casilla{
+
+	public AlaCarcelCasilla(char tipo, String nombre, int pos) {
+		super(tipo, nombre, pos);
+	}
+}
