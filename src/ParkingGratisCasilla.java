09e8127f4eb629c53d6842d8a1b1813e72769442
Jaime Ojeda González <j.ojeda.gonzalez@gmail.com>
2019-06-07 11:50:06 +0200

    Creación de casilla Parking Gratis

---
+++ src/ParkingGratisCasilla.java
@@ -0,0 +1,12 @@
+/*
+ Monopoly para cliente Inv2AS”
+ Proyectos Informáticos II - 2019
+ Grupo C (Juan Jose Garcia, Manuel Angel Mateos, Jaime Ojeda)
+ */
+
+public class ParkingGratisCasilla extends Casilla{
+
+	public ParkingGratisCasilla(char tipo, String nombre, int pos){ 
+  		super(tipo, nombre, pos);
+  	}
+}
